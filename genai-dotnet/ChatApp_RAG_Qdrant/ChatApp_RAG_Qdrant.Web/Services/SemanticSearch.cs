08044eb54fc8f9ad14b6597fa2271f7a6734c91a
RikiRicks <riky92.ce@gmail.com>
2025-10-03 15:59:12 +0530

    Add the rag using qdrant and aspire

---
+++ genai-dotnet/ChatApp_RAG_Qdrant/ChatApp_RAG_Qdrant.Web/Services/SemanticSearch.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.VectorData;
+
+namespace ChatApp_RAG_Qdrant.Web.Services;
+
+public class SemanticSearch(
+    VectorStoreCollection<Guid, IngestedChunk> vectorCollection, ILogger<SemanticSearch> logger)
+{
+    public async Task<IReadOnlyList<IngestedChunk>> SearchAsync(string text, string? documentIdFilter, int maxResults)
+    {
+        logger.LogInformation("Searching for top {maxResults} chunks matching text: {text} with documentIdFilter: {documentIdFilter}", maxResults, text, documentIdFilter ?? "null");
+        var nearest = vectorCollection.SearchAsync(text, maxResults, new VectorSearchOptions<IngestedChunk>
+        {
+            Filter = documentIdFilter is { Length: > 0 } ? record => record.DocumentId == documentIdFilter : null,
+        });
+
+        return await nearest.Select(result => result.Record).ToListAsync();
+    }
+}
